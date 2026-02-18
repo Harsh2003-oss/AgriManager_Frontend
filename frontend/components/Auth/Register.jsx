e659c60bd2a751d01456b399b9b9bef668d733b2
Harsh2003-oss <harsh.sil872@gmail.com>
2026-02-18 23:15:55 +0530

    fix: fixed bugs

--- frontend/components/Auth/Register.jsx
+++ frontend/components/Auth/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import api from '../../config/api'
+import api from '../../src/config/api'
 
 const Register = () => {
     const[name,setName] = useState('')
--- frontend/components/Crops/Crop.jsx
+++ frontend/components/Crops/Crop.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import api from '../../config/api';
+import api from '../../src/config/api'
 
 const Crops = ({ onDataChange }) => {
   const [crops, setCrops] = useState([]);
